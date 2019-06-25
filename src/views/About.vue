6ec1dc4653eccf3a86fd96f0f286b4143d39f3a9
Patrick Lamprecht <lamprecht.patrick1@gmail.com>
2019-06-25 11:40:54 +0200

    change text

--- src/views/About.vue
+++ src/views/About.vue
@@ -33,19 +33,19 @@ export default class About extends Vue {
     {
       title: "Über uns",
       text:
-        "Austrian Squadron ist ein Clan aus leiwanden Leuten, die mehrere Dinge verbindet. Partys, Lan-Partys, E-Sport und die daraus folgende Leidenschaft des Gamings. Gamerinnen und Gamer, Gelegenheitsspieler und Tryhards, Consolelover und PC Masterraceler, motivierte Leut und faule Hund, wir haben alles dabei! Hauptsache sie sind leiwand. Derzeit umfasst der Clan circa 80 Mitglieder . Das Ziel von Austrian Squadron ist es, nicht nur eine Gaming-Community aus Leuten aufzubauen, mit denen man gerne spielt, sondern auch im Real Life gemeinsam erinnerungswürdige Momente zu erleben.",
+        "Austrian Squadron ist ein Verein aus leiwanden Leuten, die mehrere Dinge verbindet. Partys, Lan-Partys, E-Sport und die daraus folgende Leidenschaft des Gamings. Gamerinnen und Gamer, Gelegenheitsspieler und Tryhards, Consolelover und PC Masterraceler, motivierte Leut und faule Hund, wir haben alles dabei! Hauptsache sie sind leiwand. Derzeit umfasst der Verein circa 80 Mitglieder . Das Ziel von Austrian Squadron ist es, nicht nur eine Gaming-Community aus Leuten aufzubauen, mit denen man gerne spielt, sondern auch im Real Life gemeinsam erinnerungswürdige Momente zu erleben. Anders als andere Organisationen, legen wir unseren Fokus nicht darauf, der größte Verein zu werden oder die meisten Preise zu holen, sondern auf eine Bereicherung des Privatlebens für alle Mitglieder.",
       img: require("@/assets/group_photo.webp")
     },
     {
       title: "Entstehung",
       img: require("../assets/evolution.png"),
       text:
-        "Die Idee einen Clan zu gründen und der Name Austrian Squadron kamen erstmals am 11.8.2016 um 4:20 in der Früh, bei einer gemütlichen Partie “Squadron TD” in Starcraft 2, zur Sprache. Man wollte ursprünglich Freundeskreise, die die Leidenschaft des E-Sport teilen, vereinen. Außerdem war Austrian Squadron einer der ersten Clans, die in der Respawn eSports Bar vertreten waren. Auch weiterhin kümmern sich einige unserer großartigen Mitglieder darum, den Clan, in puncto Modernisierung und Luxus weiter voranzutreiben."
+        "Die Idee einen Verein zu gründen und der Name Austrian Squadron kamen erstmals am 11.8.2016 um 4:20 in der Früh, bei einer gemütlichen Partie “Squadron TD” in Starcraft 2, zur Sprache. Man wollte ursprünglich Freundeskreise, die die Leidenschaft des E-Sport teilen, vereinen. Außerdem war Austrian Squadron einer der ersten Vereins, die in der Respawn eSports Bar vertreten waren. Auch weiterhin kümmern sich einige unserer großartigen Mitglieder darum, den Verein, in puncto Modernisierung und Luxus weiter voranzutreiben."
     },
     {
       title: "Wie werde ich Mitglied?",
       text:
-        "Da wir stark auf die Qualität der Mitglieder setzen (wir wollen keine Ungustln, Asoziale oder Kiddies) und nicht auf Quantität, kann man bei uns nur Mitglied werden, wenn man volljährig ist und zwei bestehende Mitglieder eine Empfehlung aussprechen. Dies ist uns besonders wichtig, da es uns lieber ist, langsam zu wachsen und mit Leuten, mit denen man Spaß bzw. eine leiwande Zeit haben kann, als auf Kosten der Community ohne zu zwinkern jeden aufzunehmen. <br><br> Daher erfolgt der Aufnahmeprozess bei uns in der Regel organisch. Sollte das Interesse einer Mitgliedschaft bestehen, kannst Du uns gerne auf Events (Respawn, Lan-Partys, etc.) oder auf unserem Discord Server besuchen und wir schauen, was sich ergibt ;)."
+        "Da wir stark auf die Qualität der Mitglieder setzen (wir wollen keine Ungustln, Asoziale, Kiddies, Sozialschwache oder zwischenmenschliche Katastrophen) und nicht auf Quantität, kann man bei uns nur Mitglied werden, wenn man volljährig ist und zwei bestehende Mitglieder eine Empfehlung aussprechen. Dies ist uns besonders wichtig, da es uns lieber ist, langsam zu wachsen und mit Leuten, mit denen man Spaß bzw. eine leiwande Zeit haben kann, als auf Kosten der Community ohne zu zwinkern jeden aufzunehmen. <br><br> Daher erfolgt der Aufnahmeprozess bei uns in der Regel organisch. Sollte das Interesse einer Mitgliedschaft bestehen, kannst Du uns gerne auf Events (Respawn, Lan-Partys, etc.) oder auf unserem Discord Server besuchen und wir schauen, was sich ergibt ;)."
     }
   ];
 }

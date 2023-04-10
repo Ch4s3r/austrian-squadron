85ac50a21546223d5376234303022fe01e935509
Ch4s3r <lamprecht.patrick1@gmail.com>
2023-04-10 18:55:05 +0200

    deploy

---
+++ precache-manifest.9a661c4702ca5e59ceb22715e567c243.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "f568b1475831cb6361e4",
+    "url": "/austrian-squadron/js/app.22e728fc.js"
+  },
+  {
+    "revision": "45045a305e2a8b190573",
+    "url": "/austrian-squadron/js/chunk-8d5a75a4.3c55ae16.js"
+  },
+  {
+    "revision": "c22c6416af1592769719",
+    "url": "/austrian-squadron/css/chunk-ab8c5b08.0eed674a.css"
+  },
+  {
+    "revision": "c22c6416af1592769719",
+    "url": "/austrian-squadron/js/chunk-ab8c5b08.4e479ad9.js"
+  },
+  {
+    "revision": "4fb6c3c663c8183c14d5",
+    "url": "/austrian-squadron/css/chunk-vendors.914c02c7.css"
+  },
+  {
+    "revision": "4fb6c3c663c8183c14d5",
+    "url": "/austrian-squadron/js/chunk-vendors.c6e10782.js"
+  },
+  {
+    "revision": "02bfbee66e3956603dfea24379559167",
+    "url": "/austrian-squadron/img/csgo.02bfbee6.png"
+  },
+  {
+    "revision": "6742ecd086f0530454906425cff7719c",
+    "url": "/austrian-squadron/img/rocketleague.6742ecd0.png"
+  },
+  {
+    "revision": "4b3639a08789f19e3b6d716246bf4876",
+    "url": "/austrian-squadron/img/overwatch.4b3639a0.png"
+  },
+  {
+    "revision": "589a885e06341cc9e5273d3aa40f1aab",
+    "url": "/austrian-squadron/img/gamers_academy_logo.589a885e.png"
+  },
+  {
+    "revision": "d37867c8680a4443ac288b0f2d202330",
+    "url": "/austrian-squadron/img/lol.d37867c8.png"
+  },
+  {
+    "revision": "b4c6c87db5b25b4bf59c40e3c39a5da2",
+    "url": "/austrian-squadron/img/group_photo.b4c6c87d.webp"
+  },
+  {
+    "revision": "917cdd21fc94c7691e9799f21e269da1",
+    "url": "/austrian-squadron/img/tischlerei_fuchs_logo_transparent.917cdd21.png"
+  },
+  {
+    "revision": "a84be1cd914709299b00320d452542e4",
+    "url": "/austrian-squadron/img/sc2.a84be1cd.png"
+  },
+  {
+    "revision": "4d7c98ef7d1740b2f471350990390a22",
+    "url": "/austrian-squadron/img/hearthstone.4d7c98ef.png"
+  },
+  {
+    "revision": "e9453cda57138eddc9ec27dca5bed66b",
+    "url": "/austrian-squadron/img/respawn_bar_logo_transparent.e9453cda.png"
+  },
+  {
+    "revision": "61f30b79daf5b31f0d254a31fba66158",
+    "url": "/austrian-squadron/fonts/fa-regular-400.61f30b79.woff2"
+  },
+  {
+    "revision": "8b0ddedbb27cbc9971c8667caa8a0cc1",
+    "url": "/austrian-squadron/fonts/fa-brands-400.8b0ddedb.woff2"
+  },
+  {
+    "revision": "20206738b2bffb741b00200d5d3d6d20",
+    "url": "/austrian-squadron/fonts/fa-regular-400.20206738.ttf"
+  },
+  {
+    "revision": "d20cedd7e254d4b58b721b6995ca52b4",
+    "url": "/austrian-squadron/fonts/fa-v4compatibility.d20cedd7.ttf"
+  },
+  {
+    "revision": "0ab3921d9b80975c5597432ab59f5d0a",
+    "url": "/austrian-squadron/fonts/fa-brands-400.0ab3921d.ttf"
+  },
+  {
+    "revision": "4bc58bc16bb05a05d3a47a4f7e143b75",
+    "url": "/austrian-squadron/fonts/fa-v4compatibility.4bc58bc1.woff2"
+  },
+  {
+    "revision": "c64278386c2bbb5e293e11b94ca2f6d1",
+    "url": "/austrian-squadron/fonts/fa-solid-900.c6427838.woff2"
+  },
+  {
+    "revision": "e2ceb83946c9e5fc7eab24453a03bffb",
+    "url": "/austrian-squadron/fonts/fa-solid-900.e2ceb839.ttf"
+  },
+  {
+    "revision": "ea7d474e4b97f83fdcddea8f22beb6bd",
+    "url": "/austrian-squadron/img/austrian-squadron-logo-white.ea7d474e.svg"
+  },
+  {
+    "revision": "e892b4aba500b701346fb95ec1569109",
+    "url": "/austrian-squadron/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/austrian-squadron/robots.txt"
+  }
+];
